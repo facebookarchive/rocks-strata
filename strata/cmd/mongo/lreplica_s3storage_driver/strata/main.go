b65afc6662075c7e7303a80d8c130fc726ad3c58
Aaron Feldman <agf@fb.com>
2015-09-03 21:14:14 +0000

    Add license headers

--- strata/cmd/mongo/lreplica_s3storage_driver/strata/main.go
+++ strata/cmd/mongo/lreplica_s3storage_driver/strata/main.go
@@ -1,8 +1,8 @@
-/*
-Package main provides a driver for the strata framework. It uses lreplica and s3storage.
+//  Copyright (c) 2015, Facebook, Inc.  All rights reserved.
+//  This source code is licensed under the BSD-style license found in the
+//  LICENSE file in the root directory of this source tree. An additional grant
+//  of patent rights can be found in the PATENTS file in the same directory.
 
-Main should be run with start-stop-daemon or a similar tool to prevent concurrent executions, which may not be safe.
-*/
 package main
 
 import (
